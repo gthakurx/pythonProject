3fc00ac7d9f53821f2ecb6579ee290cded38c2e3
gauravkumar <gthakurx@gmail.com>
2024-03-29 18:16:36 +0530

    adding initial file

---
+++ main.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import keyword
+def func(i):
+    while(i<10):
+        print(i)
+        i=i+1;
+    print(keyword.kwlist)
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    func(1)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
